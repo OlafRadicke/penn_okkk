358371669ab054d05b928b901d889f416ce5ef69
Olaf Radicke <briefkasten@olaf-radicke.de>
2009-12-13 22:29:35 +0100

    Fertig mit Indexaufteilung.

--- make_pdf.sh
+++ make_pdf.sh
@@ -2,4 +2,5 @@ pdflatex ./main.tex;
 makeindex -s ./main.ist ./main.idx;
 makeindex -s ./main.ist ./main.idx -o ./main.ind;
 makeindex -s ./main.ist ./main.adx -o ./main.and;
+makeindex -s ./main.ist ./main.bdx -o ./main.bnd;
 pdflatex ./main.tex
