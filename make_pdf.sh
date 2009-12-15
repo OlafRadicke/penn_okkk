fa219dba3bcc62de6ef8fafbd74425e12890ee0c
Olaf Radicke <briefkasten@olaf-radicke.de>
2009-12-15 10:47:15 +0100

    Weiter mit Lexikon

--- make_pdf.sh
+++ make_pdf.sh
@@ -4,3 +4,13 @@ makeindex -s ./main.ist ./main.idx -o ./main.ind;
 makeindex -s ./main.ist ./main.adx -o ./main.and;
 makeindex -s ./main.ist ./main.bdx -o ./main.bnd;
 pdflatex ./main.tex
+
+if [ $# -lt 1 ] ;
+then
+    exit 1
+else
+    if [ $1 = "-v" ] ;
+    then
+    (okular ./main.pdf)
+    fi
+fi
